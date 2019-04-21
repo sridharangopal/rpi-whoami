bfdb9ca83eccb4ac02a5258478b7f71af10812e5
SRIDHARAN GOPAL <SRIDHARAN.GOPAL@LIBERTYMUTUAL.COM>
2019-04-21 12:10:44 -0400

    added a webpage

--- http.go
+++ http.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "net/http"
-  "log"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-    fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
-    hostname, _ := os.Hostname()
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
- 	fmt.Fprintf(w, "I'm %s\n", hostname)
-    })
+	fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
+	hostname, _ := os.Hostname()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
+		fmt.Fprintf(w, "I'm %s\n", hostname)
+	})
+	//Added this to serve a webpage for the EE Days 2019 Demo
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-
-    log.Fatal(http.ListenAndServe(":" + port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
-
